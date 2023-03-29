0457d04d69afa8f7ca7bbd9225c0ca6513692fc0
LiuJC <1289095701@qq.com>
2023-03-29 11:54:42 +0800

    v1.2
    Signed-off-by: LiuJC <1289095701@qq.com>

--- app.py
+++ app.py
@@ -1,32 +1,58 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# 定义本地记事本文件路径
-NOTES_FILE = 'notes.txt'
 
-
-# 定义函数，用于读取本地记事本文件
-def read_notes():
-    with open(NOTES_FILE, 'r') as f:
-        notes = f.read().splitlines()
+def load_notes():
+    notes = []
+    with open('notes.txt', 'r') as f:
+        for line in f:
+            notes.append(line.strip())
     return notes
 
 
-# 定义函数，用于添加记录到本地记事本文件
-def add_note(note):
-    with open(NOTES_FILE, 'a') as f:
-        f.write(note + '\n')
+def save_notes(notes):
+    with open('notes.txt', 'w') as f:
+        for note in notes:
+            f.write(note + '\n')
 
 
-# 定义路由，用于显示所有记录和添加新记录
-@app.route('/', methods=['GET', 'POST'])
-def notes():
-    if request.method == 'POST':
-        note = request.form['note']
-        add_note(note)
-    notes = read_notes()
+@app.route('/')
+def index():
+    notes = load_notes()
     return render_template('index.html.jinja2', notes=notes)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    note = request.form['note']
+    notes = load_notes()
+    notes.append(note)
+    save_notes(notes)
+    return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    id = request.form['id']
+
+    try:
+        id_int = int(id)
+
+        if id_int > 0:
+            notes = load_notes()
+
+            if id_int <= len(notes):
+                notes.pop(id_int - 1)
+                save_notes(notes)
+                return redirect(url_for('index'))
+
+    except (ValueError, TypeError):
+        pass
+
+    # 显示给用户的错误消息
+    error_msg = '无法删除所选笔记，请输入有效的笔记编号。'
+    return render_template('index.html.jinja2', error_message=error_msg)
 
 
 if __name__ == '__main__':
